c60ac8aaf7ad52bd40b8ab42f251dc4580299be8
Natan Rodrigues <natanrodriguesdeoliveira@gmail.com>
2019-05-20 13:01:06 -0300

    atualizacao da declaracao dos sensores

--- upa.h
+++ upa.h
@@ -5,6 +5,6 @@ namespace sek{
 	void andar_reto();
 	void girar_esquerda();
 	void girar_direita();
-	void ultrassonico(ev3::ultrasonic ultra);
-	void cor(ev3::color color);
+	void ultrassonico(ev3::ultrasonic_sensor ultra);
+	void cor(ev3::color_sensor color);
 }
