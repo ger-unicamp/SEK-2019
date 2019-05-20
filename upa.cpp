482f3920d24fbca859b5b9ca9d589de3d665722d
Natan Rodrigues <natanrodriguesdeoliveira@gmail.com>
2019-05-20 12:43:42 -0300

    arquivos para codar pro upa

---
+++ upa.cpp
@@ -0,0 +1,3 @@
+#include "upa.h"
+
+
---
+++ upa.h
@@ -0,0 +1,10 @@
+#include "ev3dev.h"
+namespace ev3 = ev3dev;
+
+namespace sek{
+	void andar_reto();
+	void girar_esquerda();
+	void girar_direita();
+	void ultrassonico(ev3::ultrasonic ultra);
+	void cor(ev3::color color);
+}
