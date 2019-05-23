53d208b82416e63e21c4580d8fd3392834dad4cc
Heigon <h217638@dac.unicamp.br>
2019-05-23 12:28:48 -0300

    Update upa.cpp

--- upa/upa.cpp
+++ upa/upa.cpp
@@ -1,4 +1,15 @@
 #include "upa.h"
+#include <vector>
+using namespace std;
+
+int sek::ultrassonico(ev3::ultrasonic_sensor ultra){
+  vector<int> vet;
+  for(int i=0;i<10;i++){
+    vet.push_back(ultra.distance_centimeters());
+  }
+  return ((vet[4]+vet[5])/2);
+}
+
 
 void sek::andar_reto(){
 	sek::leftMotor.set_speed_sp(200).run_forever();
@@ -6,6 +17,6 @@ void sek::andar_reto(){
 }
 
 int main(){
-	
+
 	return 0;
 }
