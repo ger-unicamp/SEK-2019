45191662c5670c674046c4ff73000f6132b09277
Natan Rodrigues <natanrodriguesdeoliveira@gmail.com>
2019-06-18 16:50:52 -0300

    adicionei o tipo void nas funcoes de run_forever no robot.cpp

--- src/robot.cpp
+++ src/robot.cpp
@@ -1,11 +1,11 @@
 #include "robot.h"
 
-robot::run_forever(int speed){
+void robot::run_forever(int speed){
     this->right_motor.set_speed_sp(speed).run_forever();
     this->left_motor.set_speed_sp(speed).run_forever();
 }
 
-robot::run_forever(){
+void robot::run_forever(){
     this->right_motor.run_forever();
     this->left_motor.run_forever();
 }
